e6c34f4596d41c8d2da5c37d9903228536fe904c
MattiaAlessi <@users.noreply.github.com>
2026-09-09 00:29:31 +0200

    Added a simple input catcher

--- examples/input_test.py
+++ examples/input_test.py
@@ -1,11 +1,12 @@
 from daemon.terminal.input import Input
 from daemon.terminal.keys import Key
- 
- 
+
+
+
 while True:
     key = Input.read()
     print(repr(key))
- 
+
     if key == Key.ESC:
         print("Bye")
         break
--- src/daemon/terminal/input.py
+++ src/daemon/terminal/input.py
@@ -4,11 +4,30 @@ from .keys import Key
 
 
 class Input:
-    EXTENDED = {"H":Key.UP,
-                        "P":Key.DOWN,
-                        "K":Key.LEFT,
-                        "M":Key.RIGHT
-                        }
+    EXTENDED = {
+        "H": Key.UP,
+        "P": Key.DOWN,
+        "K": Key.LEFT,
+        "M": Key.RIGHT,
+        "G": Key.HOME,
+        "O": Key.END,
+        "I": Key.PAGE_UP,
+        "Q": Key.PAGE_DOWN,
+        "R": Key.INSERT,
+        "S": Key.DELETE,
+        ";": Key.F1,
+        "<": Key.F2,
+        "=": Key.F3,
+        ">": Key.F4,
+        "?": Key.F5,
+        "@": Key.F6,
+        "A": Key.F7,
+        "B": Key.F8,
+        "C": Key.F9,
+        "D": Key.F10,
+        "\x85": Key.F11,
+        "\x86": Key.F12,
+    }
     
     PREFIX = ("\x00", "\xe0")
 
@@ -25,8 +44,7 @@ class Input:
     def read():
         key = msvcrt.getwch()
 
-        #if key == 'à':
-        if key in Input.PREFIX and (key != "\x00" or key == "\xe0"):
+        if key in Input.PREFIX:
             code = msvcrt.getwch()
             return Input.EXTENDED.get(code, key + code)
             
