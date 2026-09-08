1c91e9e585c50e8e25b397a13aca9f6bb77113c2
MattiaAlessi <@users.noreply.github.com>
2026-09-08 20:11:21 +0200

    Added the 0.1 final color version

--- examples/colors_test.py
+++ examples/colors_test.py
@@ -1,5 +1,33 @@
 from daemon.terminal.colors import Color
 
-print(Color.RED + "Questo è rosso!" + Color.END)
-print(Color.GREEN + "Questo è verde!" + Color.END)
-print(Color.BOLD + "Questo è grassetto!" + Color.END)
+Color.red("RED")
+Color.green("GREEN")
+Color.yellow("YELLOW")
+Color.blue("BLUE")
+Color.magenta("MAGENTA")
+Color.cyan("CYAN")
+Color.white("WHITE")
+
+Color.light_red("LIGHT RED")
+Color.light_green("LIGHT GREEN")
+Color.light_yellow("LIGHT YELLOW")
+Color.light_blue("LIGHT BLUE")
+Color.light_magenta("LIGHT MAGENTA")
+Color.light_cyan("LIGHT CYAN")
+Color.light_white("LIGHT WHITE")
+
+
+Color.red(
+    "ERRORE!",
+    Color.BOLD,
+    Color.UNDERLINE
+)
+
+
+text = Color.red(
+    "Questo non viene stampato",
+    Color.BOLD,
+    printable=False
+)
+
+print(f"STAMPA SUCCESSIVA: {text}")
--- src/daemon/terminal/colors.py
+++ src/daemon/terminal/colors.py
@@ -11,9 +11,9 @@ class Color:
     DARK_GRAY = "\033[1;30m"
     LIGHT_RED = "\033[1;31m"
     LIGHT_GREEN = "\033[1;32m"
-    YELLOW = "\033[1;33m"
+    LIGHT_YELLOW = "\033[1;33m"
     LIGHT_BLUE = "\033[1;34m"
-    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_MAGENTA = "\033[1;35m"
     LIGHT_CYAN = "\033[1;36m"
     LIGHT_WHITE = "\033[1;37m"
     BOLD = "\033[1m"
@@ -24,3 +24,44 @@ class Color:
     NEGATIVE = "\033[7m"
     CROSSED = "\033[9m"
     END = "\033[0m"
+    
+    
+
+    
+    @staticmethod
+    def apply(testo, color, *styles):
+        colorful_text = color
+        for style in styles:
+            colorful_text += style
+        colorful_text += testo + Color.END
+        return colorful_text
+    
+
+    
+    @staticmethod
+    def _apply_color(testo, color, *styles, printable=True):
+        if printable:
+            print(Color.apply(testo, color, *styles))
+            return True
+        return Color.apply(testo, color, *styles)
+        
+
+
+
+
+
+def make_color_method(color):
+    def color_method(testo, *styles, printable=True):
+        return Color._apply_color(
+            testo,
+            color,
+            *styles,
+            printable=printable
+        )
+
+    return color_method
+    
+for name, code in list(vars(Color).items()):
+    if isinstance(code, str) and (code.startswith("\033[0;") or code.startswith("\033[1;")) :
+        method_name = name.lower()
+        setattr(Color, method_name, make_color_method(code))
